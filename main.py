06d610d1bbc6b78142fea4c17d25b28ab94bc4be
Sebastian Sirimarco <sesirimarco@gmail.com>
2025-10-12 13:37:00 -0300

    feat: agregar colision de muros

--- main.py
+++ main.py
@@ -3,17 +3,40 @@ import pygame
 pygame.init()
 
 
-def controls(x, y, speed):
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+def movePlayer(x, y, speed, dir):
+
+    if dir == DIR_LEFT:
         x -= speed
-    if keys[pygame.K_RIGHT]:
+    if dir == DIR_RIGHT:
         x += speed
-    if keys[pygame.K_UP]:
+    if dir == DIR_UP:
         y -= speed
-    if keys[pygame.K_DOWN]:
+    if dir == DIR_DOWN:
         y += speed
     return x, y
+
+
+DIR_LEFT = 'LEFT'
+DIR_RIGHT = 'RIGHT'
+DIR_UP = 'UP'
+DIR_DOWN = 'DOWN'
+
+
+def getDirection():
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+
+        return DIR_LEFT
+    if keys[pygame.K_RIGHT]:
+
+        return DIR_RIGHT
+    if keys[pygame.K_UP]:
+
+        return DIR_UP
+    if keys[pygame.K_DOWN]:
+
+        return DIR_DOWN
 
 
 def initStage(width, height, screen, color):
@@ -27,15 +50,34 @@ def initStage(width, height, screen, color):
                      (0, 0, width, screen.get_height()))
 
 
+def wallCollision(x, y, wall_width, wall_height, screen):
+    if x < wall_width:
+        return True
+    elif x > screen.get_width() - wall_width:
+        return True
+    elif y < wall_height:
+        return True
+    elif y > screen.get_height() - wall_height:
+        return True
+    else:
+        return False
+
+
 def init():
     screen = pygame.display.set_mode((640, 480))
     clock = pygame.time.Clock()
     SNAKE_COLOR = (255, 0, 0)
+    SNAKE_DEATH_COLOR = (238, 130, 238)
     WALL_COLOR = (255, 255, 0)
     BG_COLOR = (0, 0, 0)
-    x, y = 0, 0
-    width, height = 10, 10
-    speed = 5
+    PLAYER_WIDTH, PLAYER_HEIGHT = 10, 10
+    WALL_WIDTH = PLAYER_WIDTH * 2
+    WALL_HEIGHT = PLAYER_HEIGHT * 2
+    SPEED = PLAYER_WIDTH
+    x, y = WALL_WIDTH, WALL_HEIGHT
+
+    LAST_DIR = DIR_RIGHT
+    currentDir = None
 
     while True:
         # Process player inputs.
@@ -43,16 +85,29 @@ def init():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 raise SystemExit
-        # Falta los limites en funcion del ancho de las paredes
-        x, y = controls(x, y, speed)
+
+        if getDirection() != None:
+            LAST_DIR = getDirection()
+            currentDir = getDirection()
+        else:
+            currentDir = LAST_DIR
+
+        x, y = movePlayer(x, y, SPEED, currentDir)
 
         screen.fill(BG_COLOR)  # Fill the display with a solid color
 
-        initStage(width, height, screen, WALL_COLOR)
-        pygame.draw.rect(screen, SNAKE_COLOR, (x, y, width, height))
+        initStage(WALL_WIDTH, WALL_HEIGHT, screen, WALL_COLOR)
+
+        if (wallCollision(x, y, WALL_WIDTH, WALL_HEIGHT, screen)):
+
+            pygame.draw.rect(screen, SNAKE_DEATH_COLOR,
+                             (screen.get_width() / 2, screen.get_height() / 2, PLAYER_WIDTH * 2, PLAYER_HEIGHT * 2))
+        else:
+            pygame.draw.rect(screen, SNAKE_COLOR,
+                             (x, y, PLAYER_WIDTH, PLAYER_HEIGHT))
 
         pygame.display.flip()  # Refresh on-screen display
-        clock.tick(60)         # wait until next frame (at 60 FPS)
+        clock.tick(5)         # wait until next frame (at 60 FPS)
 
 
 init()
